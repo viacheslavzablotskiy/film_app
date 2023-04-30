64eb4a6baafbfd9fe77d6e2d5f0805c6d3805884
viacheslavzablotskiy <zlava.mag@gmail.com>
2023-04-30 22:03:39 +0300

    "refactor_location_images_videos"

--- backend/accounts/scripts_model/path_file.py
+++ backend/accounts/scripts_model/path_file.py
@@ -1,4 +1,4 @@
 def get_path_upload_avatar(instance, file):
     """ Построение пути к файлу, format: (media)/avatar/user_id/photo.jpg
     """
-    return f'avatar/user_{instance.id}/{file}'
+    return f'my_image_and_video/avatar/user_{instance.id}/{file}'
--- backend/film/path_film/get_path_upload_cover_image.py
+++ backend/film/path_film/get_path_upload_cover_image.py
@@ -1,4 +1,4 @@
 def get_path_upload_cover_image(instance, file):
     """ Построение пути к файлу, format: (media)/album/user_id/photo.jpg
     """
-    return f'image/user_{instance.user.id}/{file}'
+    return f'my_image_and_video/image/user_{instance.user.id}/{file}'
